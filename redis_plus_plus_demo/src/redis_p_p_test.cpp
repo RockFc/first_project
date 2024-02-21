27e97c55eb2daeaa3e9acb9dff66ae5d61473741
RockFc <769357879@qq.com>
2024-02-21 14:18:14 +0800

    update rpp test demo

--- redis_plus_plus_demo/src/redis_p_p_test.cpp
+++ redis_plus_plus_demo/src/redis_p_p_test.cpp
@@ -24,6 +24,33 @@ auto show_map = []( const std::string& name, const auto& m ) {
     cout << endl << endl;
 };
 
+auto show_map_17 = []( const std::string& name, const auto& m ) {
+    cout << name << " [ ";
+    for ( const auto& [ f, s ] : m )
+    {
+        cout << "(" << f << ", " << s << "), ";
+    }
+    cout << "]";
+    cout << endl << endl;
+};
+
+auto show_opt_vec = []( const std::string& name, const auto& v ) {
+    cout << name + " [ ";
+    for ( const auto& e : v )
+    {
+        if ( e )
+        {
+            cout << *e << ", ";
+        }
+        else
+        {
+            cout << "null, ";
+        }
+    }
+    cout << "]";
+    cout << endl << endl;
+};
+
 int main()
 {
 
