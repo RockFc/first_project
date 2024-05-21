c5d3909134e5b9b7da91f9faeeb85092eaebfa4e
RockFc <769357879@qq.com>
2024-05-21 20:44:04 +0800

    update

--- grammar_training/src/variable_temple.cpp
+++ grammar_training/src/variable_temple.cpp
@@ -12,7 +12,15 @@ void print(T first, Args... args) {
     print(args...); // 递归调用
 }
 
+template<typename ... T>
+auto sum(T ... t) {
+    return (t + ...);
+}
+
 int main() {
-    print(1, 2.5, "Hello", 'A', true); // 调用示例
+    print(1, 2.5, "Hello", 'A', true);
+    print(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    print(sum(1.1, 2.2));
+    print(sum(std::string("hello"),std::string(" world"))); 
     return 0;
 }
