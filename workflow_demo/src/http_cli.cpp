09186543d5c90f2520d1499f1886938d306c4a49
RockFc <769357879@qq.com>
2024-08-05 16:21:01 +0800

    update http cli demo

--- workflow_demo/src/http_cli.cpp
+++ workflow_demo/src/http_cli.cpp
@@ -1,36 +1,53 @@
-#include "workflow/WFTaskFactory.h"
 #include "workflow/WFFacilities.h"
+#include "workflow/WFTaskFactory.h"
+#include <chrono>
 #include <iostream>
+#include <thread>
+
+
 
 // 回调函数，用于处理 HTTP 响应
-void http_callback(WFHttpTask *task)
+void http_callback(WFHttpTask* task)
 {
-    protocol::HttpResponse *resp = task->get_resp();
-    const void *body;
-    size_t body_len;
+    protocol::HttpResponse* resp = task->get_resp();
+    const void*             body;
+    size_t                  body_len;
     resp->get_parsed_body(&body, &body_len);
 
-    std::cout << "HTTP Response:" << std::endl;
-    std::cout << std::string((const char *)body, body_len) << std::endl;
+    protocol::HttpRequest* req = task->get_req();
+
+    std::cout << req->get_request_uri() << " HTTP Response:" << std::endl;
+    std::cout << std::string(( const char* )body, body_len) << std::endl;
 }
 
 int main()
 {
     // 创建一个 WaitGroup，等待任务完成
-    WFFacilities::WaitGroup wait_group(1);
+    WFFacilities::WaitGroup wait_group(2);
 
     // 创建 HTTP GET 请求任务
-    WFHttpTask *task = WFTaskFactory::create_http_task("http://localhost:8888/hello", 0, 0, [&wait_group](WFHttpTask *task)
-    {
-        // 调用 http_callback 处理响应
-        http_callback(task);
-
-        // 标记任务完成
-        wait_group.done();
-    });
+    auto task_hello = WFTaskFactory::create_http_task("http://localhost:8888/hello", 0, 0,
+                                                       [ &wait_group ](WFHttpTask* task)
+                                                       {
+                                                           http_callback(task);
+                                                           wait_group.done();
+                                                       });
 
     // 启动任务
-    task->start();
+    task_hello->start();
+
+    // 等待 1 秒
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    // 创建 HTTP GET 请求任务
+    auto task_time = WFTaskFactory::create_http_task("http://localhost:8888/time", 0, 0,
+                                                         [ &wait_group ](WFHttpTask* task)
+                                                         {
+                                                             http_callback(task);
+                                                             wait_group.done();
+                                                         });
+    // 启动任务
+    task_time->start();
 
     // 等待任务完成
     wait_group.wait();
