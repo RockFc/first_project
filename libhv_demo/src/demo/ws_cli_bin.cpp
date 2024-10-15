cd9243e67dd9ff70b149dfafc66bc7ea594f2d54
RockFc <769357879@qq.com>
2024-10-15 14:07:34 +0800

    libhv demo add post delay testcase

--- libhv_demo/src/demo/ws_cli_bin.cpp
+++ libhv_demo/src/demo/ws_cli_bin.cpp
@@ -2,17 +2,13 @@
 #include <hv/HttpClient.h>
 #include <iostream>
 
-int main()
+// 创建 HttpClient 对象
+hv::HttpClient client;
+
+int HttpPostBin(const char* path, const MyStruct& original)
 {
-    // 创建 HttpClient 对象
-    hv::HttpClient client;
-
     // 设置 URL
-    const char* url = "http://localhost:10080/post_bin";
-
-    // 准备要发送的二进制数据
-    MyStruct original = {42, "Hello, World!", 3.14159};
-
+    std::string url = "http://localhost:10080/" + std::string(path);
     // 序列化结构体
     size_t dataSize;
     void*  serializedData = serialize(&original, &dataSize);
@@ -46,6 +42,21 @@ int main()
     std::cout << "Response status: " << resp->status_code << std::endl;
     MyStruct deserialized = deserialize(resp->body.data(), resp->body.size());
     printMyStruct(&deserialized);
+    return 0;
+}
 
+int main()
+{
+    // 准备要发送的二进制数据
+    MyStruct original = {42, "Hello, World!", 42.42};
+    // HttpPostBin("post_bin", original);
+    // original.intValue    = 43;
+    // original.doubleValue = 43.43;
+    // HttpPostBin("post_bin", original);
+
+    HttpPostBin("post_bin_delay", original);
+    original.intValue    = 43;
+    original.doubleValue = 43.43;
+    HttpPostBin("post_bin_delay", original);
     return 0;
 }
--- libhv_demo/src/demo/ws_svr_bin.cpp
+++ libhv_demo/src/demo/ws_svr_bin.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "ws_svr_bin.h"
+#include "hv/EventLoop.h"
 #include "hv/HttpServer.h"
 #include "hv/WebSocketServer.h"
 #include "hv/hasync.h"   // import hv::async
@@ -12,17 +13,41 @@
 
 using namespace hv;
 
-/*
- *
- * @server  bin/3t_http_svr 8080
- *
- * @client  curl -v http://127.0.0.1:10080/ping
- *          curl -v https://127.0.0.1:8443/ping --insecure
- *          bin/curl -v http://127.0.0.1:10080/ping
- *          bin/curl -v https://127.0.0.1:8443/ping
- *
- */
 #define TEST_HTTPS 0
+
+std::map<uint16_t, HttpContextPtr> m_httpContex;       // 保存http请求应答上下文
+std::mutex                         m_httpContexMutex;  // 保护http请求应答上下文
+std::atomic<uint16_t>              m_httpCtxId{0};     // http请求应答上下文自增id
+
+void AddHttpContex(uint16_t id, HttpContextPtr ctx)
+{
+    std::lock_guard<std::mutex> lock(m_httpContexMutex);
+    m_httpContex[id] = ctx;
+}
+
+bool SendData(MyStruct pack)
+{
+    auto                         id = pack.intValue;
+    std::unique_lock<std::mutex> lock(m_httpContexMutex);
+    auto                         it = m_httpContex.find(id);
+    if (m_httpContex.end() == it)
+    {
+        return false;
+    }
+    auto   ctx = it->second;
+    size_t dataSize;
+    void*  serializedData = serialize(&pack, &dataSize);
+    if (!serializedData)
+    {
+        printf("Failed to serialize data.\n");
+        return false;
+    }
+    std::string body(reinterpret_cast<const char*>(serializedData), dataSize);
+    ctx->setBody(body);
+    m_httpContex.erase(id);
+    lock.unlock();
+    return ctx->send();
+}
 
 int main(int argc, char** argv)
 {
@@ -53,7 +78,7 @@ int main(int argc, char** argv)
     };
 
     HttpService router;
-
+    auto        loop = std::make_shared<EventLoop>();
     /* Static file service */
     // curl -v http://ip:port/
     router.Static("/", "./html");
@@ -80,9 +105,8 @@ int main(int argc, char** argv)
                     printMyStruct(&deserialized);
 
                     // 模拟业务处理
-                    deserialized.intValue    = 100;
                     deserialized.stringValue = "hello,http!";
-                    deserialized.doubleValue = 300.0;
+                    deserialized.doubleValue *= 2;
 
                     // 序列化二进制数据
                     size_t dataSize2;
@@ -97,6 +121,33 @@ int main(int argc, char** argv)
                     ctx->setContentType(http_content_type::APPLICATION_OCTET_STREAM);
                     ctx->setBody(bodystr);
                     return ctx->send();
+                });
+
+    router.POST("/post_bin_delay",
+                [&loop](const HttpContextPtr& ctx)
+                {
+                    // 解析二进制数据
+                    const std::string body           = ctx->body();
+                    auto              serializedData = body.data();
+                    size_t            dataSize       = ctx->body().size();
+                    MyStruct          deserialized   = deserialize(serializedData, dataSize);
+                    printMyStruct(&deserialized);
+                    m_httpCtxId++;
+
+                    ctx->setContentType(http_content_type::APPLICATION_OCTET_STREAM);
+
+                    AddHttpContex(m_httpCtxId.load(), ctx);
+                    loop->setTimeout(5000,
+                                     [deserialized](TimerID)
+                                     {
+                                         // 模拟业务处理
+                                         MyStruct pack;
+                                         pack.intValue    = m_httpCtxId.load();
+                                         pack.stringValue = "hello,http!";
+                                         pack.doubleValue = deserialized.doubleValue * 2;
+                                         SendData(pack);
+                                     });
+                    return 0;
                 });
 
     /**
@@ -139,7 +190,8 @@ int main(int argc, char** argv)
     server.start();
 
     printf("press Enter to stop server...\n");
-    // press Enter to stop
+
+    loop->run();
     while (getchar() != '\n')
         ;
     hv::async::cleanup();
